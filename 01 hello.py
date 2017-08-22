7f1327b55b809434faadcddd7e4f16461fd5b3be
Jason Francis <jarfranc@indiana.edu>
2017-08-22 16:33:24 -0400

    First commit

---
+++ 01 hello.py
@@ -0,0 +1,13 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin
+
+import sys
+
+assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
+
+name = input("Please enter your name: ")
+print('Hello, ' + name + ', my name is Jason Francis')
+print('My favorite game right now is Spyparty')
+print('I am anxious about teaching this class for the first time, and I hope the students respond well')
+print('I am in to about board games and backpacking (and making my own backpacking gear); I like cooking and spending time with my family')
+print('My stackoverflow user number is 5338')
+print('My github profile is https://github.com/ILS-Z399')
