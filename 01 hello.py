6b28a4396be63e9c98ad1f8a340fd9096dc52893
ILS-Z399 <30897280+ILS-Z399@users.noreply.github.com>
2017-08-25 06:41:15 -0400

    Adds instructor's solution

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,9 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+print('My name is Jason Francis')
+print('My favorite video game right now is SpyParty')
+print('I am concerned about doing a good job teaching this class')
+print('I am excited about cooking and board games (and I''m looking forward to getting some sleep')
+print('My stackoverflow.com user number is 5338')
+print('My github.com profile is https://github.com/ILS-Z399')
