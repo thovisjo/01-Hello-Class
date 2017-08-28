dfbbfab518d21de3fad1241dda0f24231b58664c
ILS-Z399 <30897280+ILS-Z399@users.noreply.github.com>
2017-08-28 08:31:44 -0400

    Adds name and favorite game

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,6 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+print('Hello, my name is Jason')
+print('My favorite game is SpyParty')
+
