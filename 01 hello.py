37881be0352a58304080de390f536c2ba01faeff
thovisjo <35284895+thovisjo@users.noreply.github.com>
2018-01-17 09:16:57 -0500

    Changes complete the assignment and updates readme and Liscense

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,11 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+### Prints a descriptoion of me.
+
+print("""Hello, Class! My name is Tanner Fox Hovis-Johnson!
+My favorite game right now is Legend of Zelda: Breath of the wild.
+I have difficulty connecting programming concepts to a finished product.
+I'm excited about the snow. Less so the cold.
+9196505 is my user number of stackoverflow.
+My url for github is https://github.com/thovisjo""")
